883d3c3ddecfd31cec569acee4b9181526a277ba
Alexander Schelchere <ascellie@gmail.com>
2019-04-23 12:38:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ let happyHolidaysTo = name => `Happy holidays, ${name}!`;
 let happyHolidayTo = (holiday, name) => `Happy ${holiday}, ${name}!`;
 
 let holidayCountdown = (days, holiday) => `It's ${days} days until ${holiday}`;
+
+happyHolidays();
